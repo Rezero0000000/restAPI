bbdec836a22e8711dfc024173e64ad7707d8ae7c
Rezero <hikarisakata91@gmail.com>
2024-03-07 19:09:15 +0700

    Update setup

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,3 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+import {logger} from "./logging";
 
-export const prismaClient =  new PrismaClient();
+const prisma = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'stdout',
+        level: 'error',
+      },
+      {
+        emit: 'stdout',
+        level: 'info',
+      },
+      {
+        emit: 'stdout',
+        level: 'warn',
+      },
+    ],
+  });
+
+  prisma.$on("query", (e) => {
+    loger.info(e)
+  });
+
+  prisma.$on("error", (e) => {
+    loger.error(e)
+  });
+
+  prisma.$on("info", (e) => {
+    loger.info(e)
+  });
+
+  prisma.$on("warn", (e) => {
+    loger.warn(e)
+  });
---
+++ src/application/logger.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loger = void 0;
+const winston_1 = __importDefault(require("winston"));
+exports.loger = winston_1.default.createLogger({
+    level: "debug",
+    format: winston_1.default.format.json(),
+    transports: [
+        new winston_1.default.transports.Console({})
+    ]
+});
